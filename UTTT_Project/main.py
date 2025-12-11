c392add8131060c441ab39b7d2470ea4bf2f4ded
Treedai-lol <143515810+Treedai-lol@users.noreply.github.com>
2025-12-11 16:01:09 +0800

    added winning conditions
    unfinished

--- UTTT_Project/main.py
+++ UTTT_Project/main.py
@@ -1,9 +1,10 @@
 board = []
-for i in range(0,9):
+temp = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+for i in range(0,9): #board initialization
     board.append([])
     for j in range(0,9):
         board[i].append(9*i+j)
-def printb(bs: list):
+def printb(bs: list): #print board function
     for i in range(0,3):
         for j in range(0,3):
             for k in range(3*i,3*i+3):
@@ -12,5 +13,6 @@ def printb(bs: list):
             print("\n",end="")
         pass
     pass
-
+def BoardFinished(bs:list,index:int):
+    bs[index]
 printb(board)
