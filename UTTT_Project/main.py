c5a79937597fac6497f4915a24aa2487264841c1
Treedai-lol <143515810+Treedai-lol@users.noreply.github.com>
2025-12-11 15:49:44 +0800

    added PrintBoard function
    a crude implementation at school, will change later

--- UTTT_Project/main.py
+++ UTTT_Project/main.py
@@ -1,1 +1,16 @@
-print("hello world")
+board = []
+for i in range(0,9):
+    board.append([])
+    for j in range(0,9):
+        board[i].append(9*i+j)
+def printb(bs: list):
+    for i in range(0,3):
+        for j in range(0,3):
+            for k in range(3*i,3*i+3):
+                for l in range(3*j,3*j+3):
+                    print(bs[k][l],end=" ")
+            print("\n",end="")
+        pass
+    pass
+
+printb(board)
