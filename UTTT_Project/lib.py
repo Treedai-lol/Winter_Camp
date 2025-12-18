f44ec67bfe4d972367ca7b2ee8e282af61054bfc
treedai <wilsontien0111@gmail.com>
2025-12-18 13:47:03 +0800

    completed BoardFinished function on my linux laptop

--- UTTT_Project/lib.py
+++ UTTT_Project/lib.py
@@ -1,9 +1,8 @@
-import numpy
 class Board():
     def __init__(self,bs: list,o:bool) -> None:
         self.bs = bs
         self.o = o
-def PrintBoard(board: Board) ->None: #print board function
+def PrintBoard(board:Board) ->None: #print board function
     for i in range(0,3):
         for j in range(0,3):
             for k in range(3*i,3*i+3):
@@ -12,7 +11,7 @@ def PrintBoard(board: Board) ->None: #print board function
             print("\n",end="")
         pass
     pass
-def GetOwnerShip(board: Board,index:int,o:bool) ->list:#returns the indices of pieces owned
+def GetOwnerShip(board:Board,index:int,o:bool) ->list: #returns the indices of pieces owned
     output = []
     if o:
         for i in range(0,9):
@@ -23,11 +22,24 @@ def GetOwnerShip(board: Board,index:int,o:bool) ->list:#returns the indices of p
             if  board.bs[index][i] == 2:
                 output.append(i)
     return output
-def BoardFinished(board:Board,index:int,o:bool) ->Bool:
-    output = True
+def BoardFinished(board:Board,index:int,o:bool) ->bool:
+    output = False
+    piecelist = GetOwnerShip(board,index,o)
+    a = b = c = False
     winning = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     for i in winning:
-        numpy.union1d()
+        for j in piecelist:
+            if j == i[0]:
+                a = True
+            if j == i[1]:
+                b = True
+            if j == i[2]:
+                c = True
+        if a & b & c:
+            output = True
+    return output
+
+
         
 def BoardInit() ->Board:
     list = []
@@ -35,21 +47,21 @@ def BoardInit() ->Board:
         list.append([])
         for j in range(0,9):
             list[i].append(0)
-    list[0][0] = 1
-    list[0][1] = 1
-    list[0][2] = 1
-    list[0][3] = 1
-    list[0][4] = 1
-    list[0][5] = 1
-    list[0][6] = 1
-    list[0][7] = 1
-    list[0][8] = 1
+    list[0][0] = 0
+    list[0][1] = 0
+    list[0][2] = 0
+    list[0][3] = 0
+    list[0][4] = 0
+    list[0][5] = 0
+    list[0][6] = 2
+    list[0][7] = 2
+    list[0][8] = 2
     board = Board(list,True)
     return board
 def main():
     board = BoardInit()
     PrintBoard(board)
-    print(GetOwnerShip(board,0,True))
-    print(BoardFinished(board,0,True))
+    print(GetOwnerShip(board,0,False))
+    print(BoardFinished(board,0,False))
 if __name__ == '__main__':
     main()
